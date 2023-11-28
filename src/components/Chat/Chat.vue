a93bbee66b718f2f7931748c02e6909e041edb0e
brittadam <britt.adam@hotmail.com>
2023-11-28 19:03:30 +0100

    feat: Get messages from API

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -5,21 +5,45 @@
     let message = ref(""); //je gebruik ref voor int, string, boolean
                                    //je gebruikt {{ }} voor printen
     let allMessages= reactive({
-        data: ["Heel", "Even", "Simpel", "Doen"],
+        data: [],
     }); //je gebruikt reactive voor array en object
 
-    //function sendMessage
-    const SendMessage = () => {
-        allMessages.data.push(message.value);
-        message.value = ""; //.value is nodig bij ref
+    //function onMounted  
+    onMounted(async() => {
+       await fetchMessages();
+    });
+
+    //function fetchMessages
+    const fetchMessages = async() => {
+        const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
+        const data = await response.json();
+        allMessages.data = data;
     }
+
+
+    //function sendMessage
+    const SendMessage = async () => {
+        const response = fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                value: message.value,
+            }),
+        });
+
+        const newMessage = await response.json();
+        allMessages.data.push(newMessage); // Add the new message to the end
+        message.value = ""; // Clear the input
+    };
 
 </script>
 
 <template>
     <div>
         <ul>
-            <li v-for="m in allMessages.data">{{ m }}</li> 
+            <li v-for="m in allMessages.data" :key="m.id">{{ m.text }}</li> 
         </ul>
         <div>
             <input v-model="message" type="text" placeholder="">
